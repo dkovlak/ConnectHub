c3a340adaab43c43508c28101eead3d170bea1ea
David Kovlak <kovlakdavid4@gmail.com>
2023-10-17 19:29:13 -0700

    just

--- ConnectHub/Areas/Identity/Data/ConnectHubUser.cs
+++ ConnectHub/Areas/Identity/Data/ConnectHubUser.cs
@@ -11,30 +11,11 @@ namespace ConnectHub.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the ConnectHubUser class
 public class ConnectHubUser : IdentityUser
 {
-    public int UserID { get; set; }
 
-    [BindProperty]
-    public string Firstname { get; set; } = "";
+    public string Firstname { get; set; }
 
-    public required string Lastname { get; set; }
+    public string Lastname { get; set; }
 
-    public required string About { get; set; }
-
-    public required string Username { get; set; }
-
-    public required string Email { get; set; }
-
-    public required string Password { get; set; }
-
-    public DateTime CreatedAt { get; set; }
-
-    public DateTime LastLogin { get; set; }
-
-    public ICollection<Post> Posts { get; set; }
-
-    public byte[] ProfilePicture { get; set; }
-
-    public IEnumerable<Category> Categories { get; set; }
-
+  
 }
 
