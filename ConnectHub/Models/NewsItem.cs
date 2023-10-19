92eea6969e756b7e830c1f766d1999bdaf8b05c0
David Kovlak <kovlakdavid4@gmail.com>
2023-10-19 10:06:16 -0700

    Home page: get started, contact info, style

---
+++ ConnectHub/Models/NewsItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ConnectHub.Models
+{
+	public class NewsItem
+	{
+        public string title { get; set; }
+        public string imageUrl { get; set; }
+        public string url { get; set; }
+        public string content { get; set; }
+    }
+}
+
